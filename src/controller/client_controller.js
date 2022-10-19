54620fbaeb52322dc5f344479dbfc7b1d3312664
Sowri19 <korturti.s@northeastern.edu>
2022-10-19 12:05:24 -0400

    modifying the put method with sequeslize

--- src/controller/client_controller.js
+++ src/controller/client_controller.js
@@ -153,52 +153,98 @@ app.get("/v1/account/:id", (req, res) => {
   }
 });
 
-app.put("/client", (req, res) => {
-  const authHead = Buffer.from(
-    req.headers.authorization.split(" ")[1],
-    "base64"
-  ).toString("ascii");
-  const user = authHead.split(":")[0];
-  const pass = authHead.split(":")[1];
-  res.status(200);
-  getData({ EmailId: user }).then((result) => {
-    // checking for Emails
-    let fn = result[0].FirstName;
-    let ln = result[0].LastName;
-    let pw = result[0].Password;
-    bcrypt.compare(bCrypting(pass), pw, function (Error, valid) {
-      if (valid) {
-        console.log("Client Authenticated");
-        let updateObject = {
-          Email: user,
-          FirstNameChanged: fn,
-          LastNameChanged: ln,
-          PasswordChanged: bCrypting(req.body.PasswordChanged),
-        };
-        putData(updateObject).then((result) => {
-          console.log(result);
-          res.send({ Output: "Client Updated" });
-        });
-      } else {
-        res.status(200);
-        // res.send({ Output: "Enter correct password" });
-        res.send([bCrypting(pass), pw]);
+// app.put("/client", (req, res) => {
+//   const authHead = Buffer.from(
+//     req.headers.authorization.split(" ")[1],
+//     "base64"
+//   ).toString("ascii");
+//   const user = authHead.split(":")[0];
+//   const pass = authHead.split(":")[1];
+//   res.status(200);
+//   getData({ EmailId: user }).then((result) => {
+//     // checking for Emails
+//     let fn = result[0].FirstName;
+//     let ln = result[0].LastName;
+//     let pw = result[0].Password;
+//     bcrypt.compare(bCrypting(pass), pw, function (Error, valid) {
+//       if (valid) {
+//         console.log("Client Authenticated");
+//         let updateObject = {
+//           Email: user,
+//           FirstNameChanged: fn,
+//           LastNameChanged: ln,
+//           PasswordChanged: bCrypting(req.body.PasswordChanged),
+//         };
+//         putData(updateObject).then((result) => {
+//           console.log(result);
+//           res.send({ Output: "Client Updated" });
+//         });
+//       } else {
+//         res.status(200);
+//         // res.send({ Output: "Enter correct password" });
+//         res.send([bCrypting(pass), pw]);
+//       }
+//     });
+//   });
+// });
+
+// Put method using sequelize
+app.put("/v1/account/:id", (req, res) => {
+  try {
+    const ClientID = req.params.id;
+    const EncodedUserPass = req.headers.authorization.split(" ")[1];
+    const DecodedUserPass = Buffer.from(EncodedUserPass, "base64").toString(
+      "ascii"
+    );
+    const DecodedUser = DecodedUserPass.split(":")[0];
+    const DecodedPass = DecodedUserPass.split(":")[1];
+    User.findOne({ where: { EmailId: DecodedUser, id: ClientID } }).then(
+      (result) => {
+        if (result === null) {
+          res.status(200);
+          res.send({ Output: "Client Not Found" });
+        } else {
+          bcrypt.compare(
+            DecodedPass,
+            result.dataValues.Password,
+            function async(err, authenticated) {
+              if (authenticated) {
+                User.update(req.body, { where: { id: ClientID } }).then(
+                  (result) => {
+                    User.findOne({
+                      where: { EmailId: DecodedUser, id: ClientID },
+                    }).then((UpdatedResult) => {
+                      console.log(UpdatedResult);
+                      UpdatedResult.dataValues.Password = undefined;
+                      res.status(200).send(UpdatedResult);
+                    });
+                  }
+                );
+              } else {
+                res.status(200);
+                res.send({ Output: "Wrong password" });
+              }
+            }
+          );
+        }
       }
-    });
-  });
+    );
+  } catch (err) {
+    return res.status(400).send("Client Not Found");
+  }
 });
 
-//Error handling in json
-app.use((Error, req, res, next) => {
-  if (Error instanceof SyntaxError && Error.status === 400 && "body" in Error) {
-    let formatError = {
-      status: Error.statusCode,
-      message: Error.message,
-    };
-    return res.status(Error.statusCode).json(formatError); // Bad request
-  }
-  next();
-});
+// //Error handling in json
+// app.use((Error, req, res, next) => {
+//   if (Error instanceof SyntaxError && Error.status === 400 && "body" in Error) {
+//     let formatError = {
+//       status: Error.statusCode,
+//       message: Error.message,
+//     };
+//     return res.status(Error.statusCode).json(formatError); // Bad request
+//   }
+//   next();
+// });
 
 User.sequelize.sync().then((req) => {
   app.listen(3080, () => {
