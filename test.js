3fbe799f5253ccdfba3a5635b8ca7e347ef80f9f
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2022-10-27 04:24:30 -0400

    setting up the multer and multer-s3

--- test.js
+++ test.js
@@ -3,3 +3,22 @@ describe("Sample Test Case 1 ", () => {
     expect(true).toBe(true);
   });
 });
+aws.config.update({
+  secretAccessKey: process.env.ACCESS_SECRET,
+  accessKeyId: process.env.ACCESS_KEY,
+  region: process.env.REGION,
+});
+
+const BUCKET = process.env.BUCKET
+const s3 = new aws.S3();
+const upload = multer({
+  storage: multerS3({
+      s3: s3,
+      acl: "public-read",
+      bucket: BUCKET,
+      key: function (req, file, cb) {
+          console.log(file);
+          cb(null, file.originalname)
+      }
+  })
+})
