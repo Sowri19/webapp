602529cdc5f8b7113203db98142dd9027773072d
Satya Sowri Sampath Korturti <korturti.s@northeastern.edu>
2022-11-17 14:01:12 -0500

    adding the dynamodb to the endpoints

--- src/controller/client_controller.js
+++ src/controller/client_controller.js
@@ -5,10 +5,15 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 let mysql = require('mysql2');
-const { User, Document } = require("../../models");
+const {
+  User,
+  Document
+} = require("../../models");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { where } = require("sequelize");
+const {
+  where
+} = require("sequelize");
 const app = express();
 const saltRounds = 10;
 const bcrypt = require("bcrypt");
@@ -22,7 +27,10 @@ Document.sequelize.sync();
 const log = require("../../config/log");
 const config = require("../../config/config.js");
 const statsd = require("statsd-client");
-const sdc = new statsd({host: config.hostName_Metric, port: config.port_Metric});
+const sdc = new statsd({
+  host: config.hostName_Metric,
+  port: config.port_Metric
+});
 let start = new Date();
 // **************************************************************
 // ****************************************************************
@@ -38,7 +46,7 @@ aws.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   region: process.env.region,
 });
-console.log(process.env.BUCKET,process.env.region,"......>JSConfig file");
+console.log(process.env.BUCKET, process.env.region, "......>JSConfig file");
 // *****************************************************************************************
 
 // aws.config.update({
@@ -57,7 +65,7 @@ const upload = multer({
     bucket: BUCKET,
     key: function (req, file, cb) {
       console.log(file);
-      cb(null, Date.now().toString()+file.originalname)
+      cb(null, Date.now().toString() + file.originalname)
     }
   })
 });
@@ -66,8 +74,8 @@ const upload = multer({
 // CONFIGURING THE AWS SNS FOR THE EMAIL VERIFICATION****************************************
 var sns = new aws.SNS({});
 var dynamoDatabase = new aws.DynamoDB({
-    apiVersion: '2012-08-10',
-    region: process.env.region || 'us-east-1'
+  apiVersion: '2012-08-10',
+  region: process.env.region || 'us-east-1'
 });
 // ****************************************************************************************
 
@@ -84,7 +92,7 @@ let bCrypting = (Password) => {
   return bcrypt.hashSync(Password, bcrypt.genSaltSync(10));
 };
 
-// WE ARE NOT USING THIS CODE OF DATA VALIDATION SEQUALIZE IS TAKING CARE OF IT************
+// WE ARE NOT USING THIS CODE OF DATA VALIDATION SEQUELIZE IS TAKING CARE OF IT************
 let ValidObject = (obj) => {
   if (
     Object.keys(obj).length === 0 ||
@@ -100,10 +108,12 @@ let ValidObject = (obj) => {
 // **********************************************************************************
 
 app.get("/healthz", async (req, res) => {
-    sdc.timing('Healthz.timeout', start);
-    log.info("/healthz is working");
-    sdc.increment('endpoint.health');
-  res.send({ message: "The server is running" });
+  sdc.timing('Healthz.timeout', start);
+  log.info("/healthz is working");
+  sdc.increment('endpoint.health');
+  res.send({
+    message: "The server is running"
+  });
 });
 
 // //Adding Bcrypt to the password
@@ -136,10 +146,50 @@ app.post("/v1/account", async (req, res) => {
       LastName: req.body.LastName,
       EmailId: req.body.EmailID,
       Password: bCrypting(req.body.Password),
+      Verified: false,
     });
+    const randomnanoID = uuidv4();
+    const expiryTime = new Date().getTime();
+    var parameter = {
+      TableName: 'csye-6225',
+      Item: {
+        'Email': {
+          S: NewClient.EmailId
+        },
+        'TokenName': {
+          S: randomnanoID
+        },
+        'TimeToLive': {
+          N: expiryTime.toString()
+        }
+      }
+    };
+    try {
+      var dydb = await dynamoDatabase.putItem(parameter).promise();
+      console.log('try dynamoDatabase', dydb);
+    } catch (err) {
+      console.log('err dynamoDatabase', err);
+    }
+    
+    var msg = {
+      'username': NewClient.EmailId,
+      'token': randomnanoID
+    };
+
+    console.log(JSON.stringify(msg));
+    const params = {
+      Message: JSON.stringify(msg),
+      Subject: randomnanoID,
+      TopicArn: 'arn:aws:sns:us-east-1:981331903688:verify_email'
+    }
+
+    var publishTextPromise = await sns.publish(params).promise();
+    console.log('publishTextPromise', publishTextPromise);
+
     sdc.timing('postclient.timeout', start);
     log.info("post /v1/account is working client created");
     sdc.increment('endpoint.postclient');
+
     NewClient.Password = undefined;
     return res.status(201).send(NewClient);
   } catch (Error) {
@@ -200,11 +250,18 @@ app.get("/v1/account/:id", (req, res) => {
     );
     const DecodedUser = DecodedUserPass.split(":")[0];
     const DecodedPass = DecodedUserPass.split(":")[1];
-    User.findOne({ where: { EmailId: DecodedUser, id: ClientID } }).then(
+    User.findOne({
+      where: {
+        EmailId: DecodedUser,
+        id: ClientID
+      }
+    }).then(
       (result) => {
         if (result === null) {
           res.status(200);
-          res.send({ Output: "Client Not Found" });
+          res.send({
+            Output: "Client Not Found"
+          });
         } else {
           bcrypt.compare(
             DecodedPass,
@@ -219,9 +276,11 @@ app.get("/v1/account/:id", (req, res) => {
               } else {
                 res.status(200);
                 sdc.timing('getclient.timeout', start);
-                 log.info("get /v1/account/:id is working client wrong password");
+                log.info("get /v1/account/:id is working client wrong password");
                 sdc.increment('endpoint.getclient');
-                res.send({ Output: "Wrong Password" });
+                res.send({
+                  Output: "Wrong Password"
+                });
               }
             }
           );
@@ -278,21 +337,35 @@ app.put("/v1/account/:id", (req, res) => {
     );
     const DecodedUser = DecodedUserPass.split(":")[0];
     const DecodedPass = DecodedUserPass.split(":")[1];
-    User.findOne({ where: { EmailId: DecodedUser, id: ClientID } }).then(
+    User.findOne({
+      where: {
+        EmailId: DecodedUser,
+        id: ClientID
+      }
+    }).then(
       (result) => {
         if (result === null) {
           res.status(200);
-          res.send({ Output: "Client Not Found" });
+          res.send({
+            Output: "Client Not Found"
+          });
         } else {
           bcrypt.compare(
             DecodedPass,
             result.dataValues.Password,
-            function async(err, authenticated) {
+            function async (err, authenticated) {
               if (authenticated) {
-                User.update(req.body, { where: { id: ClientID } }).then(
+                User.update(req.body, {
+                  where: {
+                    id: ClientID
+                  }
+                }).then(
                   (result) => {
                     User.findOne({
-                      where: { EmailId: DecodedUser, id: ClientID },
+                      where: {
+                        EmailId: DecodedUser,
+                        id: ClientID
+                      },
                     }).then((UpdatedResult) => {
                       console.log(UpdatedResult);
                       UpdatedResult.dataValues.Password = undefined;
@@ -308,7 +381,9 @@ app.put("/v1/account/:id", (req, res) => {
                 sdc.timing('putclient.timeout', start);
                 log.info("put /v1/account/:id is working client wrong password");
                 sdc.increment('endpoint.putclient');
-                res.send({ Output: "Wrong password" });
+                res.send({
+                  Output: "Wrong password"
+                });
               }
             }
           );
@@ -321,9 +396,9 @@ app.put("/v1/account/:id", (req, res) => {
 });
 
 // Testing the post Method
-app.post('/upload', upload.single('file'), (req, res)=> {
+app.post('/upload', upload.single('file'), (req, res) => {
   console.log(req.file);
-  res.send("File Uploaded"+ req.file.location+ ' Location!');
+  res.send("File Uploaded" + req.file.location + ' Location!');
 });
 
 // Implementing the Post Method
@@ -336,11 +411,17 @@ app.post("/v1/document", (req, res) => {
     );
     const DecodedUser = DecodedUserPass.split(":")[0];
     const DecodedPass = DecodedUserPass.split(":")[1];
-    User.findOne({ where: { EmailId: DecodedUser} }).then(
+    User.findOne({
+      where: {
+        EmailId: DecodedUser
+      }
+    }).then(
       (result) => {
         if (result === null) {
           res.status(200);
-          res.send({ Output: "Client Not Found" });
+          res.send({
+            Output: "Client Not Found"
+          });
         } else {
           bcrypt.compare(
             DecodedPass,
@@ -367,7 +448,9 @@ app.post("/v1/document", (req, res) => {
                 sdc.timing('postclientdocument.timeout', start);
                 log.info("put /v1/document is not working wrong password");
                 sdc.increment('endpoint.postclientdocument');
-                res.send({ Output: "Wrong Password" });
+                res.send({
+                  Output: "Wrong Password"
+                });
               }
             }
           );
@@ -388,49 +471,56 @@ app.get("/v1/document/:doc_id", (req, res) => {
     );
     const DecodedUser = DecodedUserPass.split(":")[0];
     const DecodedPass = DecodedUserPass.split(":")[1];
-    User.findOne({ where: { EmailId: DecodedUser} }).then(
+    User.findOne({
+      where: {
+        EmailId: DecodedUser
+      }
+    }).then(
       (result) => {
         if (result === null) {
           res.status(200);
-          res.send({ Output: "Client Not Found" });
+          res.send({
+            Output: "Client Not Found"
+          });
         } else {
-          
+
           bcrypt.compare(
             DecodedPass,
             result.dataValues.Password,
             function (err, authenticated) {
               if (authenticated) {
                 console.log("Authenticated........................>", authenticated);
-  
+
                 // const DocId = DocID; 
                 Document.findOne({
-                where: {
-                user_id: result.dataValues.id, 
-                doc_id: DocId
-              },
-              }).then(
-              (DocResult)=>{ 
-                // console.log(DocResult, "...............>>>>>id");
-              if(DocResult){
-                sdc.timing('getclientdocument.timeout', start);
-                log.info("get /v1/document/:doc_id is working document found");
-                sdc.increment('endpoint.getclientdocument');
-                res.status(200).send(DocResult);
-              }
-              else{
-                sdc.timing('getclientdocument.timeout', start);
-                log.info("get /v1/document/:doc_id is not working Forbiden");
-                sdc.increment('endpoint.getclientdocument');
-                return res.status(403).send("Forbidden");
-              }
-            }
-            )
+                  where: {
+                    user_id: result.dataValues.id,
+                    doc_id: DocId
+                  },
+                }).then(
+                  (DocResult) => {
+                    // console.log(DocResult, "...............>>>>>id");
+                    if (DocResult) {
+                      sdc.timing('getclientdocument.timeout', start);
+                      log.info("get /v1/document/:doc_id is working document found");
+                      sdc.increment('endpoint.getclientdocument');
+                      res.status(200).send(DocResult);
+                    } else {
+                      sdc.timing('getclientdocument.timeout', start);
+                      log.info("get /v1/document/:doc_id is not working Forbiden");
+                      sdc.increment('endpoint.getclientdocument');
+                      return res.status(403).send("Forbidden");
+                    }
+                  }
+                )
               } else {
                 res.status(200);
                 sdc.timing('getclientdocument.timeout', start);
                 log.info("get /v1/document/:doc_id is not working wrong password");
                 sdc.increment('endpoint.getclientdocument');
-                res.send({ Output: "Wrong Password" });
+                res.send({
+                  Output: "Wrong Password"
+                });
               }
             }
           );
@@ -443,7 +533,7 @@ app.get("/v1/document/:doc_id", (req, res) => {
 });
 
 app.delete("/v1/document/:doc_id", async (req, res) => {
-  try{
+  try {
     // const DocId = req.params.doc_id;
     const EncodedUserPass = req.headers.authorization.split(" ")[1];
     const DecodedUserPass = Buffer.from(EncodedUserPass, "base64").toString(
@@ -453,47 +543,43 @@ app.delete("/v1/document/:doc_id", async (req, res) => {
     const DecodedPass = DecodedUserPass.split(":")[1];
     console.log(DecodedUser, "pass user");
     console.log(DecodedPass, "pass pass");
-   const Client = await User.findOne({
-    where: {
-      EmailId: DecodedUser,
-    },
-  });
-      if (Client) {
-        const CorrectPass = bcrypt.compareSync(DecodedPass, Client.Password);
-        if (CorrectPass) {    
-          const document_id = req.params.doc_id; 
-            const docx = await Document.findOne({
-              where: {
-                user_id: Client.id,
-                doc_id:document_id,
-              },
-            });
-            if (docx) {
-              await s3.deleteObject({Bucket:BUCKET,Key: docx.name}).promise();
-              const del = await Document.destroy({
-                where: {
-                  user_id: Client.id,
-                  doc_id: document_id,
-                },
-              });
-               if(del){
-                sdc.timing('deleteclientdocument.timeout', start);
-                log.info("delete /v1/document/:doc_id working Docuemnt Deleted");
-                sdc.increment('endpoint.deleteclientdocument');
-                res.status(200).send("Document Deleted");
-               }
-            }
-            else{
-              sdc.timing('deleteclientdocument.timeout', start);
-              log.info("delete /v1/document/:doc_id working Not Found");
-              sdc.increment('endpoint.deleteclientdocument');
-              return res.status(404).send("Not Found");
-            }
-         } else {
+    const Client = await User.findOne({
+      where: {
+        EmailId: DecodedUser,
+      },
+    });
+    if (Client) {
+      const CorrectPass = bcrypt.compareSync(DecodedPass, Client.Password);
+      if (CorrectPass) {
+        const document_id = req.params.doc_id;
+        const docx = await Document.findOne({
+          where: {
+            user_id: Client.id,
+            doc_id: document_id,
+          },
+        });
+        if (docx) {
+          await s3.deleteObject({
+            Bucket: BUCKET,
+            Key: docx.name
+          }).promise();
+          const del = await Document.destroy({
+            where: {
+              user_id: Client.id,
+              doc_id: document_id,
+            },
+          });
+          if (del) {
+            sdc.timing('deleteclientdocument.timeout', start);
+            log.info("delete /v1/document/:doc_id working Docuemnt Deleted");
+            sdc.increment('endpoint.deleteclientdocument');
+            res.status(200).send("Document Deleted");
+          }
+        } else {
           sdc.timing('deleteclientdocument.timeout', start);
-          log.info("delete /v1/document/:doc_id working unauthorized");
+          log.info("delete /v1/document/:doc_id working Not Found");
           sdc.increment('endpoint.deleteclientdocument');
-          return res.status(401).send("Unauthorized");
+          return res.status(404).send("Not Found");
         }
       } else {
         sdc.timing('deleteclientdocument.timeout', start);
@@ -501,12 +587,17 @@ app.delete("/v1/document/:doc_id", async (req, res) => {
         sdc.increment('endpoint.deleteclientdocument');
         return res.status(401).send("Unauthorized");
       }
+    } else {
+      sdc.timing('deleteclientdocument.timeout', start);
+      log.info("delete /v1/document/:doc_id working unauthorized");
+      sdc.increment('endpoint.deleteclientdocument');
+      return res.status(401).send("Unauthorized");
     }
-    catch(err) {
-        console.log(err);
-        return res.status(400).send("Bad Request");
-    }
-    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Bad Request");
+  }
+});
 
 // //Error handling in json
 // app.use((Error, req, res, next) => {
